cd3ab7c909e831783d56b2d5811a02b1370680c1
sagar2000000 <sagar171joshi@gmail.com>
2024-07-20 23:48:54 +0545

    admin pannel added

--- adminpannel/src/pages/List/List.jsx
+++ adminpannel/src/pages/List/List.jsx
@@ -3,7 +3,7 @@ import './List.css'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 function List({url}) {
- const url="http://localhost:4000"
+ 
 
     const fetchList=async ()=>{
       const response=await axios.get(`${url}/api/food/list`)
